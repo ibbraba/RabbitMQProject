d7d3ac77d6b271cb4a6a2c128026141df2c9ed2b
David LI <108392457+BanggEddy@users.noreply.github.com>
2025-05-20 15:16:30 +0200

    init component index done front + react dom

---
+++ client/src/components/indexcomponent.js
@@ -0,0 +1,9 @@
+const Indexcomponent = () => {
+    return (
+        <div>
+            <h1>index components</h1> 
+        </div>
+    );
+};
+
+export default Indexcomponent;
---
+++ client/src/pages/index.js
@@ -0,0 +1,12 @@
+import Indexcomponent from '../components/indexcomponent';
+
+const Index = () => {
+
+  return (
+    <div className="page-container">
+      <Indexcomponent />
+    </div>
+  );
+};
+
+export default Index;
